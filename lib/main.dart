934f6b6aff4f2aa7a0717c617af293291d5641e3
ederjgb94 <ederjgb94@gmail.com>
2023-07-13 16:06:00 -0600

    firebase cache integration concept

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker/faker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasetesting/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +12,17 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  var db = FirebaseFirestore.instance;
-  db.waitForPendingWrites();
-  db.settings = const Settings(
-    persistenceEnabled: true,
-    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
-  );
+  await GetStorage.init();
+
+  // var db = FirebaseFirestore.instance;
+  // await db.waitForPendingWrites();
+  // db.settings = const Settings(
+  //   persistenceEnabled: true,
+  //   cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+  //   host: '10.0.2.2:8080',
+  // );
+
+  FirebaseFirestore.instance.useFirestoreEmulator('10.0.2.2', 8080);
 
   runApp(const MyApp());
 }
@@ -49,37 +56,81 @@ class _MyHomePageState extends State<MyHomePage> {
   final int _counter = 0;
   var db = FirebaseFirestore.instance;
 
-  void _incrementCounter() {
+  Future<void> _incrementCounter() async {
     var db = FirebaseFirestore.instance;
+    var time = FieldValue.serverTimestamp();
 
-    // Create a new user with a first and last name
-    final user = <String, dynamic>{
-      "first": "Ada",
-      "last": "Lovelace",
-      "born": 1815
-    };
-
-// Add a new document with a generated ID
-    db.collection("users").add(user).then((DocumentReference doc) =>
-        print('DocumentSnapshot added with ID: ${doc.id}'));
+    await db.collection('users').add(
+      {
+        'first': faker.person.firstName(),
+        'last': faker.person.lastName(),
+        'timestamp': time,
+      },
+    );
+    setState(() {});
   }
 
-  obtener() {
-    int i = 0;
-    return db
-        .collection("users")
-        .snapshots(includeMetadataChanges: true)
-        .listen((querySnapshot) {
-      for (var change in querySnapshot.docChanges) {
-        if (change.type == DocumentChangeType.added) {
-          final source =
-              (querySnapshot.metadata.isFromCache) ? "local cache" : "server";
+  Future obtener() async {
+    Source CACHE = Source.cache;
+    Source SERVER = Source.server;
 
-          print("$i Data fetched from $source}");
-          i++;
-        }
-      }
-    });
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    var userCache = await db
+        .collection("users")
+        .orderBy("timestamp", descending: true)
+        .limit(1)
+        .get(
+          GetOptions(source: CACHE),
+        );
+    return userCache.docs;
+  }
+
+  Future testobtener() async {
+    Source CACHE = Source.cache;
+    Source SERVER = Source.server;
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    var box = GetStorage();
+
+    var cacheTime = box.read('cacheTime');
+
+    if (cacheTime == null) {
+      var usersServer = await db
+          .collection("users")
+          .orderBy("timestamp", descending: true)
+          .get(
+            GetOptions(source: SERVER),
+          );
+      await box.write('cacheTime',
+          usersServer.docs.first.data()['timestamp'].toDate().toString());
+      print(box.read('cacheTime').toString());
+      return usersServer.docs;
+    } else {
+      var cacheTime = box.read('cacheTime');
+      print(cacheTime.toString());
+      await db
+          .collection('users')
+          .where(
+            'timestamp',
+            isGreaterThan: Timestamp.fromDate(DateTime.parse(cacheTime)),
+          )
+          .get(
+            GetOptions(source: SERVER),
+          )
+          .then(
+              (value) => print('se trajeron del server: ${value.docs.length}'));
+
+      var users = await db
+          .collection('users')
+          .orderBy('timestamp', descending: true)
+          .get(
+            GetOptions(source: CACHE),
+          );
+      await box.write('cacheTime',
+          users.docs.first.data()['timestamp'].toDate().toString());
+      return users.docs;
+    }
   }
 
   @override
@@ -89,22 +140,40 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: testobtener(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  onTap: () async {
+                    var user = snapshot.data[index];
+                    await db.collection('users').doc(user.id).update(
+                      {
+                        'first': 'updated ${faker.person.firstName()}',
+                      },
+                    );
+                    // var user = snapshot.data[index];
+                    // await db.collection('users').doc(user.id).delete();
+                    setState(() {});
+                  },
+                  title: Text(snapshot.data[index].data()['first']),
+                  subtitle: Text(snapshot.data[index]
+                      .data()['timestamp']
+                      .toDate()
+                      .toString()),
+                );
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: obtener,
+        onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
